25ec57a4001a3b7a5a3306ba04974b3f38dd01ee
DevStuffz <67448356+DevStuffzz@users.noreply.github.com>
2020-07-09 19:18:28 -0700

    Update FlyingType.cs

--- 2d/Enemy/FlyingType.cs
+++ 2d/Enemy/FlyingType.cs
@@ -1,1 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFlyingType : MonoBehaviour
+{
+
+    public Transform playerPos;
+
+    Rigidbody2D rb;
+
+    public float moveSpeed;
+
+    public Vector2 test;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        test = rb.velocity;
+
+        float angle = Mathf.Atan2(playerPos.position.y, playerPos.position.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angle - 90)), 0.25f);
+
+        rb.velocity = new Vector2(transform.forward.x * moveSpeed, transform.forward.y * moveSpeed);
+
+    }
+}
 
